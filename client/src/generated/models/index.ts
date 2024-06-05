5a8c956bfd669eea1872b810fc073f917b56ce45
杉浦伶 <sugiura@sugiuranonotobukkukonpyuta.local>
2024-06-05 10:12:01 +0900

    feat: viteでclient初期化

---
+++ client/src/generated/models/index.ts
@@ -0,0 +1,6 @@
+export * from './patch-todos-id-request';
+export * from './post-todos-request';
+export * from './status';
+export * from './todo';
+export * from './todo-list-item';
+export * from './user';
